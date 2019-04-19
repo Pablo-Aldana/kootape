42e24cf67ea203b7a94994e237b484015a07fd04
Pablo Aldana <thorinroble@gmail.com>
2019-04-19 20:40:36 +0200

    Adjusting manifest to try PWA

--- kootape-gatsby/gatsby-config.js
+++ kootape-gatsby/gatsby-config.js
@@ -1,20 +1,20 @@
 module.exports = {
   siteMetadata: {
     title: "Kootape",
-    author: "Pablo Aldana",
-    description: "La revolución en la salud en Fitness"
+    author: "Pablo Aldana",
+    description: "Expertos en Kinesionología deportiva"
   },
   plugins: [
     'gatsby-plugin-react-helmet',
     {
       resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+      options:
+        name: 'Kootape - Expertos en Kinesionología deportiva',
+        short_name: 'Kootape',
         start_url: '/',
         background_color: '#663399',
         theme_color: '#663399',
-        display: 'minimal-ui',
+        display: 'standalone',
         icon: 'src/assets/images/website-icon.png', // This path is relative to the root of the site.
       },
     },
